4682a01ad32f4019f18dc4f314b84f42b4fd4698
Salih Keleş <keles.salih@gmail.com>
2019-04-08 19:31:52 +0300

    Preidate Renamed As Queryable

--- Net.Expressions.Test/Queryable.cs
+++ Net.Expressions.Test/Queryable.cs
@@ -5,13 +5,13 @@ using Xunit;
 
 namespace Net.Expressions.Test
 {
-    public class PredicateTest
+    public class Queryable
     {
         [Fact]
         public void Test1()
         {
             var items = new[] { "ali", "veli", "49", "50" };
-            var query = Predicate<string>.New(p => p.StartsWith("a"))
+            var query = Queryable<string>.New(p => p.StartsWith("a"))
                 .Or(p => p.EndsWith("i"));
             var result = items.Where(query.AsFunc()).ToArray();
         }
